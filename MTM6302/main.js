224795c7758d44f5dfbdf4bcc8149470d97265ba
Brennan Whitworth <whit1036@algonquinlive.com>
2023-01-19 08:03:49 -0500

    adds comments to main.js

--- MTM6302/main.js
+++ MTM6302/main.js
@@ -1,3 +1,5 @@
+// The following line demonstrates how to log a message to the console
 console.log("Hello, world!")
 
+// The following line demonstrates how to log a warning to the console
 console.warn('Take me to your leader!')
